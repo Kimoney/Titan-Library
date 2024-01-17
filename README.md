2287efdf06093132260903c900d748eaf01ecc1e
John <johntaykimani@gmail.com>
2024-01-17 19:02:41 +0300

    Feat: Rendering the search history from the local json

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Titan-Library
+
+covers for img tag
+https://covers.openlibrary.org/b/isbn/9789027401687-M.jpg
--- main.js
+++ main.js
@@ -3,13 +3,29 @@ console.log("We init")
 // We have a recent search page using a local JSON database.
 // Let's fetch that data
 const recentSearchUrl = "http://localhost:3000/recents"
+const hiiNayo = "https://openlibrary.org/search.json?q=the+lord+of+the+rings"
 
-function displayRecentSearch (url){
+function displaySearch (url){
     fetch(url)
     .then(response => response.json())
-    .then(data => console.log(data))
+    .then(data => {
+        const mine = data.docs.slice(0,3);
+        console.log(mine)
+        const myObj = mine.map(books => ({
+            id: books.key,
+            author: books.author_name,
+            title: books.title,
+            isbn: books.isbn,
+            published: books.publish_date,
+            location: books.publish_place,
+            cover: books.cover_i,
+            subject: books.subject
+        }))
+        console.log(myObj)
+        console.log(myObj[0].title)
+    })
 }
-displayRecentSearch(recentSearchUrl)
+displaySearch(hiiNayo)
 
 // Function to handle our search and make a POST request
 
@@ -33,8 +49,8 @@ function handleSearch (event){
     const searchText = document.getElementById('search-input').value;
     console.log(`You are searched for ${searchText} at ${dateString}`)
     // Save to Json with a function
-    function saveSearch (url){
-        fetch(url, {
+    async function saveSearch (url){
+        await fetch(url, {
             method: "POST",
             headers: {
                 "Content-Type":"application/json"
@@ -46,8 +62,35 @@ function handleSearch (event){
                 }
             )
                     })
-        .then(response => response.json())
-        .then(data => console.log(data))
+
     }
     saveSearch(recentSearchUrl)
 }
+
+// Write a function that loads the db.json showing the recent searches
+function displayRecentSearch (){
+    fetch(recentSearchUrl)
+    .then(response => response.json())
+    .then(data => renderRecents(data))
+// Function Handling data from fetch
+    function renderRecents(data){
+        console.log(data)
+        const myDiv = document.getElementById('recent-data');
+        
+        data.forEach(previous => {
+            let li = document.createElement('li');
+            li.id = "recent";
+            li.innerHTML = 
+            `<div id="recent-list-div">
+            <button id="recent-search-btn">${previous.recent_search}</button>
+            <span id="search-time"><i class="fa fa-clock-o" aria-hidden="true"></i> ${previous.time}</span> <i class="fa fa-trash" id="delete-icon" aria-hidden="true"></i>
+            </div>
+            `
+            jsonId= previous.id;
+            
+            myDiv.appendChild(li)
+        })
+    }
+
+}
+displayRecentSearch()
