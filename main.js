3d2d979ec3c5bf09673375194569d1b8fab1e0ec
John <johntaykimani@gmail.com>
2024-01-20 11:54:45 +0300

    Fix: removed console logs

--- main.js
+++ main.js
@@ -1,30 +1,29 @@
 function init (){
-// We have a recent search column that is using a local JSON database, the variable below holds that url
+// We have an add wishlist column that is using a local JSON database, the variable below holds that url
 const wishlistUrl = "http://localhost:3000/wishlists/"
-    // The code captures the time a search was done and saves it to our db.json
-    const currentDate = new Date();
-    const currentDayOfMonth = currentDate.getDate()
-    const currentMonth = currentDate.getMonth(); // Be careful! January is 0, not 1
-    const currentYear = currentDate.getFullYear();
-    const currentHour = currentDate.getHours();
-    const currentMinute = currentDate.getMinutes();
-    const currentSeconds = currentDate.getSeconds();
-    // The variable below combines the time to a human readable time 
-    const dateString = currentHour + ':' + currentMinute + ":" + currentSeconds + " on " + currentDayOfMonth + "-" + (currentMonth + 1) + "-" + currentYear;
+// The code captures the server time and saves it in a variable
+const currentDate = new Date();
+const currentDayOfMonth = currentDate.getDate()
+const currentMonth = currentDate.getMonth();
+const currentYear = currentDate.getFullYear();
+const currentHour = currentDate.getHours();
+const currentMinute = currentDate.getMinutes();
+const currentSeconds = currentDate.getSeconds();
+// The variable below changes the time to a human readable time 
+const dateString = currentHour + ':' + currentMinute + ":" + currentSeconds + " on " + currentDayOfMonth + "-" + (currentMonth + 1) + "-" + currentYear;
 
 function displaySearch (url){
     fetch(url)
     .then(response => response.json())
     .then(data => {
-        // From the public API, we only take a specific range from the results
+// From the public API, we only take a specific range from the results
         const myResults = data.docs.slice(0,30);
-        // The hasOwnProperty method checks whether the ISBN is available because it is vital in generating the book covers
+// The hasOwnProperty method checks whether the ISBN is available because it is vital in generating the book covers according to OpenLibrary API
         const filteredResult = myResults.filter(singleObject => singleObject.hasOwnProperty('isbn'))
-        // The condition below alerts the user that there are no search results for their query when we fail to capture and array after filtering our data
+// The condition below alerts the user that there are no search results for their query when we fail to capture an array after filtering our data
         if (filteredResult.length === 0){
-            console.log('error')
             const myDiv = document.getElementById('main-container')
-        // This code removes and replaces the DOM element with an error and another form to make a search
+// This code removes and replaces the DOM element with an error message and another button that takes the user back to the search form
             const previousState = document.getElementById('search-div')
             if(document.contains(previousState)){
                 previousState.remove()
@@ -39,7 +38,7 @@ function displaySearch (url){
             `
             myDiv.appendChild(errorDiv)
         } else {
-        // Define the properties of our new object
+// Define the properties of the new object
         const myObj = filteredResult.map(books => ({
             id: books.key,
             author: books.author_name,
@@ -47,16 +46,16 @@ function displaySearch (url){
             isbn: books.isbn[0],
             published: books.first_publish_year
         }))
-    // Create the eelements that will render the results
+// Create the elements that will render the results
         const cardContainer = document.createElement('div');
             cardContainer.id = "cards-container"
             const searchDiv = document.getElementById('search-div')
             if (document.contains(searchDiv)){
                 searchDiv.replaceWith(cardContainer)
             }
-    // Iteration of each book from our sliced and filtered results
+// Iteration of each book from our sliced and filtered results
         myObj.forEach(book => {
-    // Grab the element to append the dynamic data
+// Grab the element to append the dynamic data
             const myDiv = document.getElementById('cards-container')
             
             let resultDiv = document.createElement('div')
@@ -73,7 +72,7 @@ function displaySearch (url){
     })
 }
 
-// saveSearch is a function to save our wishlist
+// saveSearch is a function to save our wishlist from the input in the add wishlist form by making a POST request
 
 function saveWishlist (url, myWishlist, dateString){
     fetch(url, {
@@ -89,7 +88,7 @@ function saveWishlist (url, myWishlist, dateString){
                 })
 }
 
-// updateSearch is a function to save our wishlist
+// updateSearch is a function to update an existing wishlist by making a PATCH request
 
 function updateWishlist (url, dateString){
     fetch(url, {
@@ -108,54 +107,7 @@ function updateWishlist (url, dateString){
 function deleteSearchHistory (url){
     fetch(url, {method: "DELETE"})
 }
-
-// handleSearch is a function that handles our search and makes a GET request
-const searchBtn = document.getElementById('search-form');
-searchBtn.addEventListener('submit', handleSearch)
-
-function handleSearch (event){
-    event.preventDefault();
-    const searchText = document.getElementById('search-input').value;
-    const newURL = `https://openlibrary.org/search.json?q=${searchText}`
-    displaySearch(newURL)
-}
-
-// Function that will saves our wishlists after filling the form
-
-const wishlistForm = document.getElementById('wishlist-form')
-wishlistForm.addEventListener('submit', handleWishlistForm)
-console.log('after save wishlist btn')
-function handleWishlistForm (event){
-    event.preventDefault()
-    console.log('after prevent default')
-    const myWishlist = document.getElementById('wishlist-text').value;
-    fetch(wishlistUrl)
-    .then(response => response.json())
-    .then(data => checkIfWishlistExist(data))
-// The Function checkIfWishlistExist checks whether a book already exists in the wishlist. If it exists a PATCH request is made, else a POST Request is made
-    function checkIfWishlistExist(data){
-        const existingElementIndex = data.findIndex(wish => wish.wishlist === myWishlist)
-        console.log(existingElementIndex)
-        console.log(data)
-        const singleWishlistObj = data[existingElementIndex]
-        // console.log(singleWishlistObj)
-        // console.log(patchUrl)
-        if (singleWishlistObj === -1) {
-            saveWishlist(wishlistUrl, myWishlist, dateString)
-// The else if below prevents a TypeError because the last else throws an undefined due to the .id (Took me a whole day to figure out)
-        } else if (singleWishlistObj === null || singleWishlistObj=== undefined){
-            saveWishlist(wishlistUrl, myWishlist, dateString)
-        }
-        else{
-            let patchUrl = wishlistUrl+singleWishlistObj.id
-            alert(`${myWishlist} already exists!`)
-            updateWishlist(patchUrl, dateString)
-            }
-    }
-}
-
-
-// Write a function that loads the db.json showing the recent searches
+// displayWishlists is a function that loads the db.json showing the recent searches
 function displayWishlists (){
     fetch(wishlistUrl)
     .then(response => response.json())
@@ -188,6 +140,51 @@ function displayWishlists (){
 
 }
 displayWishlists()
+
+// handleSearch is a function that handles data from the search form and sends a query to the public API by making a GET request
+
+const searchBtn = document.getElementById('search-form');
+searchBtn.addEventListener('submit', handleSearch)
+
+function handleSearch (event){
+    event.preventDefault();
+    const searchText = document.getElementById('search-input').value;
+    const newURL = `https://openlibrary.org/search.json?q=${searchText}`
+    displaySearch(newURL)
+}
+
+// The event listener below listens for the wishlist form submission
+
+const wishlistForm = document.getElementById('wishlist-form')
+wishlistForm.addEventListener('submit', handleWishlistForm)
+
+// Function that will save our wishlists after filling the wishlist form
+
+function handleWishlistForm (event){
+    event.preventDefault()
+    const myWishlist = document.getElementById('wishlist-text').value;
+    fetch(wishlistUrl)
+    .then(response => response.json())
+    .then(data => checkIfWishlistExist(data))
+
+// The Function checkIfWishlistExist checks whether a book already exists in the wishlists json database. If it exists a PATCH request is made, else a POST Request is made
+
+    function checkIfWishlistExist(data){
+        const existingElementIndex = data.findIndex(wish => wish.wishlist === myWishlist)
+        const singleWishlistObj = data[existingElementIndex]
+        if (singleWishlistObj === -1) {
+            saveWishlist(wishlistUrl, myWishlist, dateString)
+// The else if below prevents a TypeError because the last else throws an undefined due to the .id (Took me a whole day to figure out!!)
+        } else if (singleWishlistObj === null || singleWishlistObj === undefined){
+            saveWishlist(wishlistUrl, myWishlist, dateString)
+        }
+        else{
+            let patchUrl = wishlistUrl+singleWishlistObj.id
+            alert(`${myWishlist} already exists!`)
+            updateWishlist(patchUrl, dateString)
+            }
+    }
+}
 }
 
 document.addEventListener("DOMContentLoaded", init)
